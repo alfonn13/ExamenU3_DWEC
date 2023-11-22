86919d9e7ce230fab7490092a7151b550699dc5a
unknown <alfonsoreviejovalle@alumnos.tuniversformacion.es>
2023-11-22 10:43:15 +0100

    Favicon and Readme

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
+/*
+Nombre: Alfonso Reviejo Valle
+GitHub: https://github.com/alfonn13/ExamenU3_DWEC
+
+*/
+
 import {loop} from "./canvas-setup.js" 
 
 document.addEventListener("DOMContentLoaded" ,()=>{
